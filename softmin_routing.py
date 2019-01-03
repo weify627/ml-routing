b6c152115dd2dcff0950a5dc3349ce415bf92131
Klint Qinami <kq2129@columbia.edu>
2019-01-03 18:28:09 -0500

    update requirements

--- softmin_routing.py
+++ softmin_routing.py
@@ -1,16 +1,30 @@
 import networkx as nx
 
 
-def create_example_graph():
+def create_example():
+    G = nx.Graph()
+    G.add_nodes_from([0, 1, 2])
+    G.add_weighted_edges_from([(0, 1, 2), (1, 2, 4), (0, 2, 7)])
 
-    return
+    D = np.array([[0,2,7],
+                  [0,0,2],
+                  [0,0,0]])
+
+    return G, D
 
 
-def softmin_routing(E, D, edge_weights):
+def softmin_routing(G, D):
+    '''
+    Return a routing policy given a directed graph with weighted edges and a
+    deman matrix.
+    args:
+        G is a networkx graph with nodes and edges with weights.
+        D is a demand matrix, represented as a 2D numpy array.
+    '''
 
     return
 
 
 if __name__ == '__main__':
-    E, D, edge_weights = create_example_graph()
-    softmin_routing(E, D, edge_weights)
+    G, D = create_example_graph()
+    softmin_routing(G, D)
