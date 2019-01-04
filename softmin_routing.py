603147a169aec29857289306eea3368cf852fb63
Klint Qinami <kq2129@columbia.edu>
2019-01-04 12:44:59 -0500

    untested softmin routing implementation

--- softmin_routing.py
+++ softmin_routing.py
@@ -1,7 +1,13 @@
 import sys
 import numpy as np
-import networkx as nx
+from collections import defaultdict
 from matplotlib import pyplot as plt
+
+import networkx as nx
+import gurobipy as gb
+
+import lp
+
 
 
 def create_example():
@@ -9,7 +15,7 @@ def create_example():
     G.add_nodes_from([0, 1, 2])
     G.add_weighted_edges_from([(0, 1, 2), (1, 2, 4), (0, 2, 7)])
 
-    D = np.array([[0,2,7],
+    D = np.array([[0,2,3],
                   [0,0,2],
                   [0,0,0]])
 
@@ -45,12 +51,33 @@ def draw_graph(G):
     return
 
 
-def _
+def split_ratio(G, s, u, t, gamma, sps):
+    sp3 = lambda a, b, c: G[a][b]['weight'] + sps[b][c]
+    num = np.exp(-gamma * sp3(s, u, t))
 
-    return
+    denom = 0.0
+    for v in G.neighbors(s):
+        denom += np.exp(-gamma * sp3(s, v, t))
+
+    if (denom == 0):
+        return 0.
+
+    return num / denom
 
 
-def softmin_routing(G, D, gamma=2):
+def get_shortest_paths(G):
+    nV = G.number_of_nodes()
+    sps = np.full((nV, nV), fill_value=np.inf)
+
+    for i in range(nV):
+        for j in range(nV):
+            if nx.has_path(G, i, j):
+                sps[i][j] = nx.shortest_path_length(G, i, j, weight='weight')
+
+    return sps
+
+
+def softmin_routing(G, D, gamma=2, verbose=False):
     '''
     Return a routing policy given a directed graph with weighted edges and a
     deman matrix.
@@ -70,37 +97,82 @@ def softmin_routing(G, D, gamma=2):
     '''
     nV = G.number_of_nodes()
     nE = G.number_of_edges()
-    F = np.zeros((nV, nV, nV, nV))
+    sps = get_shortest_paths(G)
 
-    for i in range(nV):
-        for j in range(nV):
-            if D[i, j]:
-                sp = []
+    m = gb.Model('netflow')
 
-                for k, l in enumerate(G.neighbors(i)):
-                    w_il = G[i][l]['weight']
+    verboseprint = print
 
-                    if nx.has_path(G, l, j):
-                        splj = nx.shortest_path_length(G, l, j, weight='weight')
-                        sp.append((l, splj + w_il))
-                    else:
-                        sp.append((l, 'inf'))
+    if not verbose:
+        verboseprint = lambda *a: None
+        m.setParam('OutputFlag', False )
+        m.setParam('LogToConsole', False )
 
-                denom = 0
-                denom = sum([np.exp(-gamma * t[1]) if (t[1]!='inf') else 0
-                             for t in sp]
-                             )
+    # Make string array for hashing
+    V = np.array([i for i in G.nodes()])
+    arcs = gb.tuplelist(G.edges())
 
-                for tup in sp:
-                    e = (i, tup[0])
-                    w = np.exp(-gamma * tup[1])
-                    F[i][j][i][l] = w / denom
+    # Create variables
+    f = m.addVars(V, V, arcs, lb=0.0, name='flow')
+    g = m.addVars(V, V, lb=0.0, name='traf_at_node')
 
-    print(F)
+    for s, t in lp.cartesian_product(V, V):
+        qs = gb.quicksum(
+                g[u, t]*split_ratio(G, u, v, t, gamma, sps)
+                for (u, v) in G.in_edges(s)
+        )
+        m.addConstr(
+            g[s, t] == qs + D[s, t],
+            'split_ratio_{}_{}'.format(s, t)
+        )
 
-    return
+    for s, t in lp.cartesian_product(V, V):
+        m.addConstr(g[s, t] == (f.sum('*', t, '*', s) + D[s, t]))
+
+    # Flow conservation constraints
+    for s, t, u in lp.cartesian_product(V, V, V):
+        d = D[int(s), int(t)]
+        if u==s:
+            m.addConstr(f.sum(s, t, u, '*')-f.sum(s, t, '*', u)==d, 'conserv')
+        elif u==t:
+            m.addConstr(f.sum(s, t, u, '*')-f.sum(s, t, '*', u)==-d, 'conserv')
+        else:
+            m.addConstr(f.sum(s, t, u, '*')-f.sum(s, t, '*', u)==0, 'conserv')
+
+    # Set objective to max-link utilization (congestion)
+    #max_cong = m.addVar(name='congestion')
+    # m.addConstrs(((cost[i,j]*l[i, j])/capacity[i,j]<=max_cong for i, j in arcs))
+
+    # Compute optimal solution
+    m.optimize()
+
+    # Print solution
+    if m.status == gb.GRB.Status.OPTIMAL:
+        f_sol = m.getAttr('x', f)
+        verboseprint('\nOptimal traffic flows.')
+        verboseprint('\nf_{i -> j}(s, t) denotes amount of traffic from source'
+                     ' s to destination t that goes through link (i, j) in E.')
+        for s, t in lp.cartesian_product(V, V):
+            for i,j in arcs:
+                p = f_sol[s, t, i, j]
+                if p >= 0:
+                    verboseprint('f_{%s -> %s}(%s, %s): %g bytes.'
+                                  % (i, j, s, t, p))
+
+        g_sol = m.getAttr('x', g)
+        verboseprint('\nTotal traffic at node.')
+        for s, t in lp.cartesian_product(V, V):
+            p = g_sol[s, t]
+            if p >= 0:
+                verboseprint('g({}, {}): {} bytes.'.format(s, t, p))
+        #m_cong = float(max_cong.x)
+        #verboseprint('\nMax. weighted link util: ', format(m_cong, '.4f'))
+    else:
+        verboseprint('\nERROR: Flow Optimization Failed!', file=sys.stderr)
+        return None, None, None
+    return f_sol#, l_sol, m_cong
 
 
 if __name__ == '__main__':
     G, D = create_example()
-    softmin_routing(G, D)
+    softmin_routing(G, D, gamma=2, verbose=True)
